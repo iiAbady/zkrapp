4edd39395177fdcf2388517cfa2afab1a03b2635
Abady <gamersspeaks@gmail.com>
2019-06-18 06:22:26 +0300

    fix: check by Azkar#last_sended

--- src/models/Time.ts
+++
@@ -1,9 +0,0 @@
-import { Entity, PrimaryColumn } from 'typeorm';
-
-/* eslint-disable @typescript-eslint/explicit-member-accessibility */
-
-@Entity('time')
-export class Time {
-	@PrimaryColumn({ type: 'timestamptz' })
-	triggers_at!: Date;
-}
