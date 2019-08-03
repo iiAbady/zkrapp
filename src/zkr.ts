597115559a4c8447abc4da57f812c8cc27aec407
Abady <gamersspeaks@gmail.com>
2019-08-03 07:21:44 +0300

    refactor: server implementation

--- src/zkr.ts
+++ src/zkr.ts
@@ -1,12 +1,19 @@
 interface TwitError { message: string; statusCode: string; code: string; twitterReply: string; allErrors: Array<TwitError> }
 
 import './util/Env';
-import Client from './client/ZkrClient';
-import server from './server';
 import { Logger } from 'winston';
+import { join } from 'path';
+import * as express from 'express';
+import session from './middleware/session';
+import Client from './client/ZkrClient';
+import Server from './server/ZkrServer';
+
+
+const app = express();
 
 const { consumer_key, consumer_secret, access_token, access_secret, PORT } = process.env;
 const client = new Client(consumer_key!, consumer_secret!, access_token!, access_secret!);
+const server = new Server(PORT!);
 
 client.stream('statuses/sample')
 	.on('connected', (): Logger => client.logger.info(`[CONNECTED: CLIENT] Zkr App is connected and ready to go!`))
@@ -14,7 +21,13 @@ client.stream('statuses/sample')
 	.on('warning', (msg): Logger => client.logger.warn(`[WARN: CLIENT] ${msg}`))
 	.on('error', (error: TwitError): Logger => client.logger.error(`[ERROR: CLIENT] ${error.message}\nTwitter Reply:${error.twitterReply}`));
 
-process.on('unhandledRejection', (err: any): Logger => client.logger.error(`[UNHANDLED REJECTION] ${err.message}`, err.stack));
+app
+	.set('views', join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.use(express.static(join(__dirname, '..', 'static')))
+	.use(session);
 
-server(PORT!);
+server.init(app);
 client.start();
+
+process.on('unhandledRejection', (err: any): Logger => client.logger.error(`[UNHANDLED REJECTION] ${err.message}`, err.stack));
