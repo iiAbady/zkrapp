80d0ce83c28655a34cb316a10897bdcf1906309f
Abady <gamersspeaks@gmail.com>
2019-08-03 07:16:25 +0300

    chore: seperate session to another folder

---
+++ src/middleware/session.ts
@@ -0,0 +1,8 @@
+import * as session from 'express-session';
+
+export default session({
+	resave: true,
+	saveUninitialized: true,
+	secret: process.env.session_secret!,
+	cookie: { maxAge: 6048e5, secure: process.env.NODE_ENV === 'production' }
+});
