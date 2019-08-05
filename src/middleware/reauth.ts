f45670fede73706c12f00d7de4fd5f429fad9c9b
Abady <gamersspeaks@gmail.com>
2019-08-05 03:51:08 +0300

    fix: reauth

--- src/middleware/reauth.ts
+++ src/middleware/reauth.ts
@@ -3,7 +3,7 @@ import Route from '../structures/Route';
 
 export default function reAuthHandler(file: Route): RequestHandler {
 	return async (_, res, next) => {
-		if (file.auth && !res.locals.user) {
+		if (file.auth && (!res.locals.user || !res.locals.user.admin)) {
 			return res.redirect(`/connect`);
 		}
 		next();
