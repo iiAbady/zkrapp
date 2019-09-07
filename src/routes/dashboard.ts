c1091f17525ee29eb7bb5a5650d91cc4e157ba93
Abady <gamersspeaks@gmail.com>
2019-09-08 02:32:40 +0300

    chore: advance azkar sorting

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -15,7 +15,11 @@ export default class LogoutRoute extends Route {
 	public async exec(req: Request, res: Response): Promise<void> {
 		const azkar = await this.db!.getRepository(Azkar).find() || [];
 		// @ts-ignore
-		azkar.sort((a, b) => a.approved - b.approved);
+		azkar.sort((a, b) => {
+			if (a.approved === b.approved) return a.id - b.id;
+			else if (a.approved) return -1;
+			1;
+		});
 		res.render('pages/dashboard', { rows: azkar });
 	}
 }
